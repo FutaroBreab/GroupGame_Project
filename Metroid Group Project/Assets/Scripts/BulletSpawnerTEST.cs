8d1aa0b302f016bf6951f7e08b82ece60e5c21eb
HunterTW <huntbrandt@gmail.com>
2025-04-14 12:12:28 -0700

    Changed Bullet Spawner Script
    Changed Bullet Spawner Script to allow for multiple bullet types

--- Metroid Group Project/Assets/Scripts/BulletSpawnerTEST.cs
+++ Metroid Group Project/Assets/Scripts/BulletSpawnerTEST.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BulletSpawnerTEST : MonoBehaviour
 {
-
+    //Set these bullet types to a bullet prefab
     public GameObject bullet;
+    public GameObject heavyBullet;
+    
+
 
 
     // Start is called before the first frame update
@@ -17,6 +20,8 @@ public class BulletSpawnerTEST : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //If X is held, shoot once, and then start to rapid fire after held
         if(Input.GetKeyDown(KeyCode.X))
         {
             Instantiate(bullet, transform.position, transform.rotation);
@@ -29,10 +34,22 @@ public class BulletSpawnerTEST : MonoBehaviour
 
     }
 
-
-    void SpawnBullet()
+    //Spawns a bullet
+    void SpawnBullet(int BulletType)
     {
-        Instantiate(bullet, transform.position, transform.rotation);
+        
+        //BulletType Index:
+        //0 = Regular Bullet
+        //1 = Heavy Bullet
+
+        if (BulletType == 0)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+        else if (BulletType == 1)
+        {
+            Instantiate(heavyBullet, transform.position, transform.rotation);
+        }
     }
 
 }
