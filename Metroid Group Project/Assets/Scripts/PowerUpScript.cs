d930d69c2a089eabf99cf934b4aff53b61a80d7f
ConnorJEdgar <connorjedgar@gmail.com>
2025-04-16 13:44:41 -0700

    PowerUp work

--- Metroid Group Project/Assets/Scripts/PowerUpScript.cs
+++ Metroid Group Project/Assets/Scripts/PowerUpScript.cs
@@ -5,25 +5,55 @@ using UnityEngine;
 public class PowerUpScript : MonoBehaviour
 {
     public PlayerScript playerScript;
+    public BulletSpawner bulletSpawner;
     public int xHealth;
     public int xJump;
     private int MaxHealth;
+    public bool MaxHealthPowerUp = false;
+    public bool MaxJumpPowerUp = false;
+    public bool HeavyBulletsPowerUp = false;
 
     private void Start()
     {
         MaxHealth = playerScript.playerHealth;
     }
 
-    //Checks if player interacts with health pack.
+    //Checks if player interacts with standard health pack.
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PlayerScript>())
         {
-            Destroy(gameObject);
+            if(MaxJumpPowerUp == true)
+            {
+                //increases jump amount twice as much
+                playerScript.jumpStrength *= 2;
+            }
             
-            playerScript.playerHealth += xHealth;
 
-            playerScript.jumpStrength += xJump;
+            //Check if max power up is checked. If not checked it should default to standard healthpack
+            if(MaxHealthPowerUp == true)
+            {
+                //increases max health and fully heals.
+                playerScript.playerHealth = MaxHealth + 100;
+            } else if (MaxHealth > playerScript.playerHealth)
+            {
+                playerScript.playerHealth += xHealth;
+
+                //Supposed to keep health from going over. Definetely easier way to do this. Clamp?
+                if (playerScript.playerHealth > MaxHealth)
+                {
+                    playerScript.playerHealth = MaxHealth;
+                }
+
+            }
+            
+            //Script to change bullets to heavy. Likely checking a bool if on in player script. Reference when available.
+            if(HeavyBulletsPowerUp == true)
+            {
+                bulletSpawner.bulletType = 1;
+            }
+
+            Destroy(gameObject);
         }
     }
 
